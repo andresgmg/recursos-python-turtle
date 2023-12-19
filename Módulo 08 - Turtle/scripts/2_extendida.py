462c4dd5fa0204b2570b174f80746f832f72944b
Andres Marquez <72410509+andresgmg@users.noreply.github.com>
2023-12-19 16:58:37 -0300

    leccion 2 lista

--- Módulo 08 - Turtle/scripts/2_extendida.py
+++ Módulo 08 - Turtle/scripts/2_extendida.py
@@ -19,33 +19,45 @@ class Tortuga(turtle.Turtle):
     def rectangulo(self, x, y, ancho, alto, color="black"):
         self.color(color)
         self.penup()
-        self.goto(x-ancho/2, y-alto/2)  # vertice superior izquierda
+        self.goto(x - ancho / 2, y - alto / 2)  # vertice superior izquierda
         self.pendown()
-        self.goto(x+ancho/2, y-alto/2)  # vertice superior derecho
-        self.goto(x+ancho/2, y+alto/2)  # vertice inferior derecho
-        self.goto(x-ancho/2, y+alto/2)  # vertice inferior izquierdo
-        self.goto(x-ancho/2, y-alto/2)  # vertice superior izquierdo
+        self.goto(x + ancho / 2, y - alto / 2)  # vertice superior derecho
+        self.goto(x + ancho / 2, y + alto / 2)  # vertice inferior derecho
+        self.goto(x - ancho / 2, y + alto / 2)  # vertice inferior izquierdo
+        self.goto(x - ancho / 2, y - alto / 2)  # vertice superior izquierdo
 
     def poligono(self, lados, radio, color="black"):
         self.color(color)
         angulo = math.pi * 2 / lados
-        ancho_ventana = window.screensize()[0]/2
-        alto_ventana = window.screensize()[1]/2
+        ancho_ventana = window.screensize()[0] / 2
+        alto_ventana = window.screensize()[1] / 2
+        # teorema del seno y del coseno para dibujar el poligono
         x = radio * math.sin(angulo) + ancho_ventana
         y = radio * math.cos(angulo) + alto_ventana
         self.penup()
         self.goto(x, y)
         self.pendown()
-        for i in range(lados+1):
-            x = radio * math.sin(angulo*i) + ancho_ventana
-            y = radio * math.cos(angulo*i) + alto_ventana
+        for i in range(lados + 1):
+            x = radio * math.sin(angulo * i) + ancho_ventana
+            y = radio * math.cos(angulo * i) + alto_ventana
             self.goto(x, y)
 
 
 kame = Tortuga()
 
-for n in range(3, 25):
-    kame.poligono(n, n*10)
+# para mostrar la funcion del rectangulo
+# x, y = (250, 250)
+# w, h = (50, 50)
+# incrementar = 50
+# for color in ["yellow", "blue", "red"] * 2:
+#     kame.rectangulo(x, y, w, h, color=color)
+#     w += incrementar
+#     h += incrementar
 
+# para mostrar la funcion del poligono
+# los colores los multiplicamos por un numero alto para que no se acabe ese bucle
+colores = ["yellow", "blue", "red"] * 100
+for n, color in zip(range(4, 20), colores):
+    kame.poligono(n, n * 10, color=color)
 
 turtle.mainloop()
